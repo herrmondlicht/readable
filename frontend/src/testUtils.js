6b0c40b96046f35c88a12840645eabab7bace192
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-05-08 23:27:47 -0300

    test(helper): adiciona abstração de comandos de teste
    #NoIssue

---
+++ frontend/src/testUtils.js
@@ -0,0 +1,18 @@
+import { shallow as enzymeShallow, mount as enzymeMount, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import React from 'react'
+import { PropTypes } from "prop-types";
+
+configure({ adapter: new Adapter() });
+
+export const shallow = (ReactComponent) => {
+  return {
+    withProps: (props) => enzymeShallow(<ReactComponent  {...props} />)
+  }
+}
+
+export const mount = (ReactComponent) => {
+  return {
+    withProps: (props) => enzymeMount(<ReactComponent  {...props} />),
+  }
+}
