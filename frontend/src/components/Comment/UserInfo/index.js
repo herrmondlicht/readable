ca17bc1cfd66b9e002bbcd1129f780fd47e95662
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-05-15 21:36:41 -0300

    refactor(comment): altera estilos
    Altera css dos componentes de Comment
    
    #NoIssue

--- frontend/src/components/Comment/UserInfo/index.js
+++ frontend/src/components/Comment/UserInfo/index.js
@@ -7,8 +7,8 @@ export const createUserInfo = (React) => {
 
   const UserInfo = ({ author, timestamp= new Date(), voteScore}) => (
     <div className={"user-info"}>
-      <span className='user-info__author'>{author} • </span>
-      <span className='user-info__timespan'>{moment(timestamp).fromNow()} • </span>
+      <span className='user-info__author'>{author}</span>
+      <span className='user-info__timespan'>{moment(timestamp).fromNow()}</span>
       <span className='user-info__votescore'>{voteScore} pts</span>
     </div>
   )
