8412ea29d2ab9eb62964f6f0c3d28342c9a6863b
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-06-03 20:43:13 +0000

    refactor(PostList): altera estrutura para comportar ListWithVoteScore
    Altera a estrutura antiga para comportar a estrutura com ListWithVoteScore, mantendo o componente mais reutilizável
    
    #NoIssue

--- frontend/src/components/Post/PostList/index.js
+++ frontend/src/components/Post/PostList/index.js
@@ -1,36 +1,34 @@
 import React from 'react'
-import { string, number } from "prop-types";
-import PostListItem from "./PostListItem";
+import { func, array } from "prop-types";
+import _PostListItem from "./PostListItem";
 import './index.css'
 import withVoteScore from "../../VoteScoreComposer";
-
-export const createPostList = React => {
+import ListWithVoteScore from '../../ListWithVoteScore'
+export const createPostList = (React, PostListItem) => {
 
   class PostList extends React.PureComponent {
 
-    getVoteScoreFunction = (postId, option) =>
-      () =>
-        this.props.voteScoreHandler && this.props.voteScoreHandler(postId, option)
+    voteScoreFunction = (postId, option) =>
+      this.props.voteScoreHandler && this.props.voteScoreHandler(postId, option)
 
     render() {
       const { postList = [] } = this.props;
       return (
         <div>
-          {postList.map(post => (
-            <div key={post.id} className="post-list__item">
-              {
-                withVoteScore(
-                  {
-                    scoreUp: this.getVoteScoreFunction(post.id, 'upVote'),
-                    scoreDown: this.getVoteScoreFunction(post.id, 'downVote')
-                  },
-                  <PostListItem post={post} />)
-              }
-            </div>
-          ))}
+          <ListWithVoteScore
+            Component={PostListItem}
+            list={postList}
+            voteScoreHandler={this.voteScoreFunction}
+            type="post"
+          />
         </div>
       )
     }
+  }
+
+  PostList.propTypes = {
+    voteScoreHandler: func,
+    postList: array,
   }
 
   return PostList
@@ -38,4 +36,4 @@ export const createPostList = React => {
 }
 
 
-export default createPostList(React)
+export default createPostList(React, _PostListItem)
--- frontend/src/components/Post/PostList/index.test.js
+++ frontend/src/components/Post/PostList/index.test.js
@@ -6,101 +6,29 @@ import { shallow } from "../../../testUtils";
 import moment from 'moment';
 
 describe('PostList', () => {
-  const PostList = createPostList(React);
+  const PostListItem = stub(),
+    PostList = createPostList(React, PostListItem);
 
   it('must be a function', () => {
     assert.isFunction(PostList, 'createPostList must return a function');
   })
 
-  it('rendering of PostListItem with correct props when prop PostList passed', () => {
-    const props = {
-      postList: [
-        {
-          id: '8xf0y6ziyjabvozdd253nd',
-          timestamp: 1467166872634,
-          title: 'Udacity is the best place to learn React',
-          body: 'Everyone says so after all.',
-          author: 'thingtwo',
-          category: 'react',
-          voteScore: 6,
-          deleted: false,
-          commentCount: 2
-        },
-        {
-          id: '6ni6ok3ym7mf1p33lnez',
-          timestamp: 1468479767190,
-          title: 'Learn Redux in 10 minutes!',
-          body: 'Just kidding. It takes more than 10 minutes to learn technology.',
-          author: 'thingone',
-          category: 'redux',
-          voteScore: -5,
-          deleted: false,
-          commentCount: 0
-        }
-      ]
-    },
-      wrapper = shallow(PostList).withProps(props);
-    props.postList.map((post, index) => {
-      const actual = wrapper.find('PostListItem').at(index).props().post;
-      assert.deepEqual(post, actual, 'PostList must have PostListItems with correct props');
-    });
-  });
-
-  it('rendering of VoteScore with correct props for each PostListItem', () => {
-    const props = {
-      postList: [
-        {
-          id: '8xf0y6ziyjabvozdd253nd',
-          timestamp: 1467166872634,
-          title: 'Udacity is the best place to learn React',
-          body: 'Everyone says so after all.',
-          author: 'thingtwo',
-          category: 'react',
-          voteScore: 6,
-          deleted: false,
-          commentCount: 2
-        },
-        {
-          id: '6ni6ok3ym7mf1p33lnez',
-          timestamp: 1468479767190,
-          title: 'Learn Redux in 10 minutes!',
-          body: 'Just kidding. It takes more than 10 minutes to learn technology.',
-          author: 'thingone',
-          category: 'redux',
-          voteScore: -5,
-          deleted: false,
-          commentCount: 0
-        }
-      ]
-    },
-      wrapper = shallow(PostList).withProps(props);
-    props.postList.map((post, index) => {
-      const actual = wrapper.find('VoteScore').at(index).props();
-      assert.isFunction(actual.scoreUp, "VoteScore's scoreUp must be a function")
-      assert.isFunction(actual.scoreDown, "VoteScore's scoreDown must be a function");
-    });
-  });
-
-  it('return of getVoteScoreFunction function', () => {
-    const voteScoreHandler = stub(),
-      wrapper = shallow(PostList).withProps({ voteScoreHandler }),
-      getVoteScoreFunction = wrapper.instance().getVoteScoreFunction,
-      actual = getVoteScoreFunction()
-    assert.isFunction(actual, 'getVoteScoreFunction() must return a function')
-  })
-
-  it('call of the returned function from getVoteScoreFunction', () => {
-    const voteScoreHandler = stub(),
-      optionParam = 'upVote',
-      postId = '8xf0y6ziyjabvozdd253nd',
-      wrapper = shallow(PostList).withProps({ voteScoreHandler }),
-      getVoteScoreFunction = wrapper.instance().getVoteScoreFunction,
-      expected = 'correct return';
-    voteScoreHandler.withArgs(postId, optionParam).returns(expected);
-    const actual = getVoteScoreFunction(postId, optionParam)();
-
-    assert.equal(actual, expected)
-
+  it('rendering of a ListWithVoteScore with correct props', () => {
+    const list = ['array', 'of', 'objects'],
+      type = 'post',
+      props = {
+        postList: list,
+      },
+      wrapper = shallow(PostList).withProps(props),
+      voteScoreFunction = wrapper.instance().voteScoreFunction,
+      actual = wrapper.find('ListWithVoteScore').props(),
+      expected = {
+        Component: PostListItem,
+        list: list,
+        voteScoreHandler: voteScoreFunction,
+        type
+      }
+    assert.deepEqual(actual, expected, "render() must render a ListWithVoteScore with correct props")
   })
 
 })
