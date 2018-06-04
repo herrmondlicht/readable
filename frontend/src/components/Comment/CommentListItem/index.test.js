dc51ea13104254f396c47be25c6305b28276f493
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-06-04 19:21:22 +0000

    fix(CommentListItem): corrige testes quebrados
    Corrige os teste que estavam quebrando por causa das alterações estruturais
    
    #NoIssue

--- frontend/src/components/Comment/CommentListItem/index.test.js
+++ frontend/src/components/Comment/CommentListItem/index.test.js
@@ -12,23 +12,26 @@ describe('Comment', () => {
   it('must render the comment in its text', () => {
     const body = 'test comment',
       wrapper = shallow(Comment).withProps({
-        body,
+        comment: { body },
       }),
       actual = wrapper.text()
     assert.include(actual, body, 'Could not render the comment')
   })
 
   it('must render a UserInfo component', () => {
-    const props = {
+    const comment = {
       body: 'test comment',
       author: 'Joshua Down',
       voteScore: -10,
       timestamp: 1525205449912,
     },
+      props = {
+        comment
+      },
       wrapper = shallow(Comment).withProps(props),
       actual = wrapper.find('UserInfo').props()
 
-    assert.deepInclude(props, actual, 'Could not render a UserInfo')
+    assert.deepInclude(comment, actual, 'Could not render a UserInfo')
   })
 
 })
